9f529ad6e3e546873a60f91847ce2b91d9d24df1
André Nascimento <andre.neospace@gmail.com>
2024-05-18 13:15:05 -0300

    navbar

---
+++ src/parts/navbar.php
@@ -0,0 +1,33 @@
+<nav class="navbar">
+  <div class="navbar-content">
+    <a href="https://flowbite.com/" class="logo">
+        <span class="self-center text-2xl font-semibold whitespace-nowrap">Digital</span>
+        <img src="assets/img/logo.png" class="h-8" alt="DigitalPlus Logo" />
+    </a>
+    <button data-collapse-toggle="navbar-default" type="button" class="navbar-toggle" aria-controls="navbar-default" aria-expanded="false">
+        <span class="sr-only">Open main menu</span>
+        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+        </svg>
+    </button>
+    <div class="hidden" id="navbar-default">
+      <ul class="font-medium flex flex-col md:items-center p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white">
+        <li>
+          <a href="#" class="nav-item active">Home</a>
+        </li>
+        <li>
+          <a href="#" class="nav-item">Sobre</a>
+        </li>
+        <li>
+          <a href="#" class="nav-item">Serviços</a>
+        </li>
+        <li>
+          <a href="#" class="nav-item">Contato</a>
+        </li>
+        <li>
+            <button class="btn btn-outline-primary">Contrate agora</button>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
