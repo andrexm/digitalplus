6b1d95f4bd65c1ba1b963f0ed9668851c2dcb308
André Nascimento <andre.neospace@gmail.com>
2024-05-18 19:03:27 -0300

    change background

--- src/parts/cta.php
+++ src/parts/cta.php
@@ -1,4 +1,5 @@
-<div class="px py2 bg-gradient-to-r from-secondary-400 to-primary-400">
+<div style="background: url('assets/img/bg3.jpg'); background-size: cover;">
+<div class="px py2 bg-white/30">
     <div class="text-center text-white">
         <div class="title mb-2">Que tal começarmos?</div>
         <div class="text-lg mb-6">Com os nossos produtos e serviços a sua equipe conseguirá transformar os seus objetivos mais ambiciosos em realidade!</div>
@@ -6,4 +7,5 @@
             <button class="btn btn-outline-white">Contrate já</button>
         </div>
     </div>
+</div>
 </div>
--- src/parts/footer.php
+++ src/parts/footer.php
@@ -1,4 +1,4 @@
-<div style="background: url('assets/img/bg2.jpg'); background-size: cover; background-position: center">
+<div style="background: url('assets/img/bg2.jpg'); background-size: cover">
     <div class="px py bg-white/30 text-white text-center">
         Copyright &copy; DigitalPlus. All rights Reserved.<br>
         Design: <a href="https://github.com/andrexm">andrexm</a>
