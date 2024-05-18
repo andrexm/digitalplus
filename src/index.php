6738802197c3db885ab1d36fe09c311a4f78d655
André Nascimento <andre.neospace@gmail.com>
2024-05-18 14:11:08 -0300

    cards

--- src/index.php
+++ src/index.php
@@ -6,10 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>New Tailwind CSS project</title>
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </head>
 <body>
     <?php include($parts . "navbar.php") ?>
     <?php include($parts . "hero.php") ?>
+    <?php include($parts . "cards.php") ?>
 
     <script src="assets/scripts/script.js"></script>
 </body>
